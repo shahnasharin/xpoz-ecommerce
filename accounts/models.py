49cff7338c4560f2cc7a19865b6aad3ae068ba10
Shahna-shifu <shahnasharinpp@gmail.com>
2023-01-02 17:24:10 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,165 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not username:
+            raise ValueError('User must have an username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+
+
+
+class Account(AbstractBaseUser):
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=50)
+
+    # required
+
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now_add=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active       = models.BooleanField(default=False)
+    is_superadmin   = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+
+   
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+# # Create your models here.
+
+# class MyAccountManager( BaseUserManager):
+#     def create_user(self,first_name,last_name,phone_number,email,password=None):
+#         if not email:
+#             raise ValueError('user must have email address')
+#         if not phone_number:
+#             raise ValueError('User must have a phone number')
+#         user= self.model(
+#             email    = self.normalize_email(email),
+#             phone_number = phone_number,
+#             first_name=first_name,
+#             last_name=last_name,         
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self,first_name,last_name,email,phone_number,password):
+#         user = self.create_user(
+#             email    = self.normalize_email(email),
+#             phone_number=phone_number,
+#             first_name=first_name,
+#             last_name=last_name,
+#             password=password,
+#         )
+#         user.is_admin =True
+#         user.is_active=True
+#         user.is_staff=True
+#         user.is_superadmin=True
+#         user.save(using=self._db)
+#         return user
+
+
+
+# class Account(AbstractBaseUser):
+#     first_name    =models.CharField(max_length=50)    
+#     last_name     =models.CharField(max_length=50)
+#     email         =models.EmailField(max_length=100,unique=True)
+#     phone_number  =models.CharField(max_length=10,blank=False,unique=True)
+#     # username      =models.CharField(max_length=50)
+
+#     date_jointed =models.DateTimeField(auto_now_add=True)
+#     last_login   =models.DateTimeField(auto_now_add=True)
+#     is_admin     =models.BooleanField(default=False)
+#     is_staff     =models.BooleanField(default=False)
+#     is_active    =models.BooleanField(default=False)
+#     is_superadmin=models.BooleanField(default=False)
+
+#     USERNAME_FIELD ='email'
+#     REQUIRED_FIELDS= ['phone_number','first_name','last_name']
+#     objects= MyAccountManager()
+#     def __str__(self):
+#         return self.email
+
+#     def has_perm(self,perm,obj=None):
+#         return self.is_admin
+
+#     def has_module_perms(self,add_labal):
+#         return True
+    
+#     def full_name(self):
+#         return(str(self.first_name+self.last_name))
+
+# class Address(models.Model):
+#     user=models.ForeignKey(Account,on_delete=models.CASCADE)
+#     address_line_1=models.CharField(max_length=100)
+#     address_line_2=models.CharField(max_length=100,blank=True)
+#     city=models.CharField(max_length=20)
+#     district=models.CharField(max_length=20)
+#     state=models.CharField(max_length=20)
+#     country=models.CharField(max_length=20)
+#     pin_code=models.IntegerField(null=True)
+#     primary_address=models.BooleanField(default=True)
+#     order_note = models.CharField(max_length=500,default='')
+
+
+#     def __str__(self):
+#         return self.address_line_1
+
+#     def full_address(self):
+#         return f'{self.address_line_1} {self.address_line_2}'
---
+++ store/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from category.models import category
+
+from django.db.models import Q
+from carts.views import CartItem
+
+from carts.views import _cart_id
+# Create your views here.
+
+# def store(request):
+#     return render (request, 'store/store.html')
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+
+    if category_slug != None:
+        categories = get_object_or_404(category,slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        
+        Product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        Product_count= products.count()
+
+    context = {
+        'products': products,
+        'Product_count': Product_count,
+
+    }
+    return render(request,'store/store.html',context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+       
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+        'in_cart'       : in_cart,
+    }
+
+    return render (request,'store/product_detail.html',context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+             products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+             product_count = products.count()
+    context = {
+        'products': products,
+        'Product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+    
+
